9583e1b94a1b2bd4d3ea9edeba24e184049e8efa
SmallMain <smallmain@outlook.com>
2022-05-20 11:31:56 +0800

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'smallmain', // Usually your GitHub org/user name.
   projectName: 'cocos-service-pack', // Usually your repo name.
-  deploymentBranch: 'master',
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
